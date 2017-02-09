1503891f144b5b74da9d5a42c6bacd479a7a2ca1
nestora103 <nestora103@gmail.com>
2017-02-09 17:28:43 +0300

    add MFU class for Task3

---
+++ src/com/gubenkoDM/dz/dz4/multiThread1/MFU.java
@@ -0,0 +1,16 @@
+package com.gubenkoDM.dz.dz4.multiThread1;
+
+/**
+ * Created by DmitriX on 09.02.2017.
+ */
+public class MFU {
+    private final int outPeriod=50;
+
+    private void print(String str){
+
+    }
+
+    private void scan(){
+
+    }
+}
