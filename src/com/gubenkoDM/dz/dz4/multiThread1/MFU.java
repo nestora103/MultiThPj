b2ee2214bc4696ef728045d1e6ad28db057eac6c
nestora103 <nestora103@gmail.com>
2017-02-10 13:32:02 +0300

    add MFU class for execute Task3

--- src/com/gubenkoDM/dz/dz4/multiThread1/MFU.java
+++ src/com/gubenkoDM/dz/dz4/multiThread1/MFU.java
@@ -5,12 +5,34 @@ package com.gubenkoDM.dz.dz4.multiThread1;
  */
 public class MFU {
     private final int outPeriod=50;
+    private final Object monPr=new Object();//монитор синхронизации задач печати
+    private final Object monSc=new Object();//монитор синхронизации задач сканирования
 
-    public void print(int printPageNumber){
-
+    public void print(int printPageNumber,int printOrderNumber){
+        synchronized (monPr){
+            try {
+                System.out.println("[Номер задания печати]:"+printOrderNumber);
+                for (int i = 0; i < printPageNumber ; i++) {
+                    System.out.println("Отпечатано страниц:" + (i + 1)+" {"+printOrderNumber+"}");
+                    Thread.sleep(outPeriod);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
-    public void scan(int scanPageNumber){
-
+    public void scan(int scanPageNumber,int scanOrderNumber){
+        synchronized (monSc){
+            try {
+                System.out.println("[Номер задания сканирования]:"+scanOrderNumber);
+                for (int i = 0; i < scanPageNumber ; i++) {
+                    System.out.println("Отсканировано страниц:" + (i + 1)+" {"+scanOrderNumber+"}");
+                    Thread.sleep(outPeriod);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
